9d6af367e7f6aae2686acadb419894430258b2b1
Gero <gersonhoa@gmail.com>
2022-01-04 12:32:20 -0300

    50%

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import Header from './components/Header';
+import SolarSystem from './components/SolarSystem';
 
 class App extends React.Component {
   render() {
-    return (<p>Sistema Solar</p>);
+    return (
+      <main>
+        <Header />
+        <SolarSystem />
+        <p>Sistema Solar</p>
+      </main>);
   }
 }
 
