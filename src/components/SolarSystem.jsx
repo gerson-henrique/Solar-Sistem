e194af8abb57e29937221c045fa24ae5abb1b719
Gero <gersonhoa@gmail.com>
2022-01-04 13:01:50 -0300

    80%

--- src/components/SolarSystem.jsx
+++ src/components/SolarSystem.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import Title from './Title';
+import PlanetCard from './PlanetCard';
+import planets from '../data/planets';
 
 class SolarSystem extends React.Component {
   render() {
     return (
       <div data-testid="solar-system">
         <Title headline="Planetas" />
+        {planets.map((Plt) => {
+          console.log('teste de funcionamento');
+          return (<PlanetCard
+            key={ Plt.name }
+            planetName={ Plt.name }
+            planetImage={ Plt.image }
+          />);
+        })}
       </div>);
   }
 }
